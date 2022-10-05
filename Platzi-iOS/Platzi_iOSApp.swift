ea16c216216b3bf62f160f9dce69e72506738654
Alan Maldonado <maldonadoalanivan@gmail.com>
2022-10-04 22:45:56 -0300

    Initial Commit

---
+++ Platzi-iOS/Platzi_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Platzi_iOSApp.swift
+//  Platzi-iOS
+//
+//  Created by Alan on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Platzi_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
